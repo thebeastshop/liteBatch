115a86b0db3e16d17162c8f7a5bf79b4caeef748
paul.xiong <paul.xiong@thebeastshop.com>
2017-03-23 18:18:15 +0800

    优化数据库类型操作

--- liteBatch-core/src/main/java/com/litesalt/batch/handler/DBRowBatchHandler.java
+++ liteBatch-core/src/main/java/com/litesalt/batch/handler/DBRowBatchHandler.java
@@ -11,16 +11,12 @@ package com.litesalt.batch.handler;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
@@ -30,7 +26,6 @@ import com.litesalt.batch.annotation.AliasField;
 import com.litesalt.batch.annotation.AliasTable;
 import com.litesalt.batch.annotation.ExcludeField;
 import com.litesalt.batch.context.HandlerContext;
-import com.litesalt.batch.entity.DBColumnMetaData;
 import com.litesalt.batch.util.CamelCaseUtils;
 import com.litesalt.batch.util.Reflections;
 
@@ -45,8 +40,6 @@ public class DBRowBatchHandler<T> extends RowBatchHandler<T> {
 
 	private Field[] fields;
 
-	private Map<String, DBColumnMetaData> metaMap = new HashMap<String, DBColumnMetaData>();
-
 	// ================private==================
 	private void prepareSql() {
 		StringBuffer sql = new StringBuffer();
@@ -54,7 +47,7 @@ public class DBRowBatchHandler<T> extends RowBatchHandler<T> {
 		sql.append(" insert into ").append(getAliasTable(clazz));
 		sql.append("(");
 		int m = 0;
-		for (Field field : clazz.getDeclaredFields()) {
+		for (Field field : fields) {
 			ExcludeField excludeField = field.getAnnotation(ExcludeField.class);
 			if (excludeField == null) {
 				m++;
@@ -73,23 +66,6 @@ public class DBRowBatchHandler<T> extends RowBatchHandler<T> {
 		}
 		sql.append(")");
 		insertSql = sql.toString();
-	}
-
-	private boolean initDBMetaData() {
-		boolean flag = false;
-		try {
-			Class<T> clazz = context.getClazz();
-			DatabaseMetaData metaData = jdbcTemplate.getDataSource().getConnection().getMetaData();
-			ResultSet rs = metaData.getColumns(null, "%", getAliasTable(clazz), "%");
-			while (rs.next()) {
-				metaMap.put(rs.getString("COLUMN_NAME"), new DBColumnMetaData(rs.getString("COLUMN_NAME"), rs.getInt("DATA_TYPE"), rs.getObject("COLUMN_DEF")));
-			}
-			flag = true;
-		} catch (Exception e) {
-			logger.error("init db metadata wrong", e);
-			flag = false;
-		}
-		return flag;
 	}
 
 	private String getAliasTable(Class<T> clazz) {
@@ -116,12 +92,10 @@ public class DBRowBatchHandler<T> extends RowBatchHandler<T> {
 		super(context);
 		this.jdbcTemplate = jdbcTemplate;
 
-		if (initDBMetaData()) {
-			Class<T> clazz = context.getClazz();
-			fields = clazz.getDeclaredFields();
+		Class<T> clazz = context.getClazz();
+		fields = clazz.getDeclaredFields();
 
-			prepareSql();
-		}
+		prepareSql();
 	}
 
 	@Override
@@ -134,54 +108,30 @@ public class DBRowBatchHandler<T> extends RowBatchHandler<T> {
 				public void setValues(PreparedStatement ps, int i) throws SQLException {
 					T t = batchList.get(i);
 					Object o = null;
-					DBColumnMetaData metaData = null;
 					int n = 0;
 					for (Field field : fields) {
 						ExcludeField excludeField = field.getAnnotation(ExcludeField.class);
 						if (excludeField == null) {
 							n++;
 							o = Reflections.invokeGetter(t, field.getName());
-							metaData = metaMap.get(getAliasField(field));
-
-							// 如果值为null，还要看默认值，如果有默认值，取元数据中的默认值
-							if (o == null) {
-								if (metaData.getColumnDef() == null) {
-									ps.setNull(n, Types.NULL);
-									continue;
-								}
-								o = metaData.getColumnDef();
-							}
-
-							switch (metaData.getDataType()) {
-							case Types.CHAR:
-							case Types.VARCHAR:
-							case Types.NVARCHAR:
+							if (o instanceof String) {
 								ps.setString(n, (String) o);
-								break;
-							case Types.BLOB:
-							case Types.LONGVARBINARY:
+							} else if (o instanceof byte[]) {
 								ps.setBytes(n, (byte[]) o);
-								break;
-							case Types.TINYINT:
-							case Types.SMALLINT:
-								ps.setShort(n, Short.parseShort(o.toString()));
-								break;
-							case Types.INTEGER:
-								ps.setInt(n, Integer.parseInt(o.toString()));
-								break;
-							case Types.BIGINT:
-								ps.setLong(n, Long.parseLong(o.toString()));
-								break;
-							case Types.DATE:
-								ps.setDate(n, new java.sql.Date(((Date) o).getTime()));
-								break;
-							case Types.TIMESTAMP:
+							} else if (o instanceof Short) {
+								ps.setShort(n, (Short) o);
+							} else if (o instanceof Integer) {
+								ps.setInt(n, (Integer) o);
+							} else if (o instanceof Long) {
+								ps.setLong(n, (Long) o);
+							} else if (o instanceof Date) {
 								ps.setTimestamp(n, new Timestamp(((Date) o).getTime()));
-								break;
-							case Types.DECIMAL:
+							} else if (o instanceof BigDecimal) {
 								ps.setBigDecimal(n, (BigDecimal) o);
-								break;
+							} else {
+								ps.setNull(n, Types.NULL);
 							}
+
 						}
 					}
 				}
